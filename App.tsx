e5e3d9d40640813b6d7c10a820586b03a8dbba0f
lucirs7 <luciars787@gmail.com>
2024-05-06 19:46:58 +0200

    Video #48. Added main logic variables and functions.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,41 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+import { FontAwesome } from '@expo/vector-icons';
+import { useEffect, useState } from 'react';
+
+let items = new Array(25).fill('empty');
+
 export default function App() {
+  const [randomNumber, setRandomNumber] = useState('');
+
+  const generateRandomNumber = () => {
+    //TODO Generate random number
+  };
+
+  useEffect(() => {
+    generateRandomNumber();
+  }, []);
+
+  const scratchItem = () => {
+    //TODO Decide lucky or unlucy
+  };
+
+  const scratchItemIcon = () => {
+    //TODO Find right icon
+  };
+
+  const scratchItemColour = () => {
+    //TODO Find right colour
+  };
+
+  const showAllItem = () => {
+    //TODO Button - reveal all icons
+  };
+
+  const resetGame = () => {
+    //TODO Reset the game
+  };
+
   return (
     <View style={styles.container}>
       <Text>Scratch and win!</Text>
